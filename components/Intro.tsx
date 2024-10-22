472113df256918c7cbf929cd50d0fdf51124956b
Kuntal616 <sadhukhank245@gmail.com>
2024-10-22 21:20:40 +0530

    changes done in intro section

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -6,16 +6,14 @@ const Intro = () =>{
     return(
         <div className="flex gap-4 flex-col-reverse sm:flex-row justify-center items-center">
             <div className="flex flex-col gap-4 md:w-9/12 sm:w-full">
-               <h1 className="text-4xl font-bold">Hi! I'm Kuntal Sadhukhan.</h1> 
+               <h1 className="text-4xl font-bold">Hi ! Kuntal Here 👋</h1> 
                <p className="text-sm text-gray-700 dark:text-gray-300">
-               A passionate and enthusiastic web developer with a strong foundation in HTML, CSS, and JavaScript. I also have experience working with frameworks like React, and I'm continuously expanding my knowledge in backend technologies.
-               I have a deep interest in building intuitive, responsive web applications.
-                {/* Although I'm just starting out, */}
-                 I'm eager to apply my skills in real-world projects and contribute to innovative solutions.This portfolio showcases some of the personal projects and exercises I've worked on, reflecting my journey as I grow in this exciting field.
+               I'm a<span className=" text-purple-700"> FullStack Web Developer</span>  with a strong foundation in HTML, CSS, and JavaScript. I also have experience working with frameworks like React, and I'm continuously expanding my knowledge in backend technologies.
                </p>
+               <p className="text-sm  text-gray-600 dark:text-gray-400 flex items-center"><Image src="/images/placeholder.svg" alt="location-icon" height={16} width={16} className=" border-gray-100  dark:border-gray-800" />Kolkata,India.</p>
             </div>
             <div className="md:w-3/12 sm:w-full ">
-                <Image src="/images/hero.png" alt="Kuntal" height={100} width={100} className=" rounded-full border border-gray-100 h-36 w-36" />
+                <Image src="/images/hero.png" alt="Kuntal" height={100} width={100} className=" rounded-full border border-gray-100  dark:border-gray-800 h-36 w-36" />
             </div>
             
         </div>
